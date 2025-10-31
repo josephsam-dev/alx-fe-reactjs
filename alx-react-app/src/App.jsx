452f5250b0d847f486e097bf986ce5263160cd6e
josephsam-dev <josephsegzy@gmail.com>
2025-10-31 22:41:24 +0100

    Edited App.jsx to show custom message

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, Joseph! My first React app is working!</h1>
+      <p>🎉 Welcome to React + Vite!</p>
+      <p>Edit <code>src/App.jsx</code> and save to see updates live.</p>
+    </div>
+  )
+}
+
+export default App

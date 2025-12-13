7fc1920c12c541112878f9658ab46e5c8e68a63e
Samuel <josephsegzy@gmail.com>
2025-12-13 13:56:14 +0100

    Add advanced routing: nested, protected, and dynamic routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,17 +1,31 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute"; // Auth wrapper
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./components/ProfileSettings";
+import UserProfile from "./components/UserProfile";
+import BlogPost from "./components/BlogPost"; // Dynamic blog route
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
-        <Route path="profile" element={<Profile />}>
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
+        <Route path="users/:userId" element={<UserProfile />} /> {/* Dynamic route */}
+
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          {/* Profile with nested routes */}
+          <Route path="profile" element={<Profile />}>
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
+
+          {/* Blog post dynamic route */}
+          <Route path="blog/:id" element={<BlogPost />} />
         </Route>
       </Routes>
     </Router>
---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,7 @@
+// src/components/BlogPost.jsx
+import { useParams } from "react-router-dom";
+
+export default function BlogPost() {
+  const { id } = useParams();
+  return <h2>Blog Post ID: {id}</h2>;
+}

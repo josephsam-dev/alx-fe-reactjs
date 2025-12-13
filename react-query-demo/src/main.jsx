6bf3337bef252e54c4b7a208eab0c73215f13931
Samuel <josephsegzy@gmail.com>
2025-12-13 12:54:19 +0100

    Move React Query setup to App.jsx for ALX checker

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,18 +1,6 @@
 import React from "react";
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import PostsComponent from "./components/PostsComponent.jsx"; // <-- import your component
+import App from "./App.jsx";
 import "./index.css";
 
-// Create a QueryClient instance
-const queryClient = new QueryClient();
-
-// Render the app
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <PostsComponent />
-    </QueryClientProvider>
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
